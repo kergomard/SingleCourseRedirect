cb4fc179bdb3dbf0bb079acd48d928d685fdc301
Stephan Winiker <stephan.winiker@hslu.ch>
2018-05-23 16:36:30 +0200

    Initial Commit

---
+++ classes/class.ilSingleCourseRedirectPlugin.php
@@ -0,0 +1,74 @@
+<?php
+require_once './Services/EventHandling/classes/class.ilEventHookPlugin.php';
+
+/**
+ * Class ilSingleCourseRedirectPlugin
+ * 
+ *
+ *
+ * @author  Stephan Winiker <webmaster@subclauses.net>
+ * @version 1.0.0
+ */
+class ilSingleCourseRedirectPlugin extends ilEventHookPlugin {
+
+	/**
+	 * @var
+	 */
+	protected static $instance;
+
+
+	/**
+	 * @return ilSingleCourseRedirectPlugin
+	 */
+	public static function getInstance() {
+		if (!isset(self::$instance)) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+
+	const PLUGIN_NAME = 'SingleCourseRedirect';
+
+
+	/**
+	 * Handle the event
+	 *
+	 * @param    string        component, e.g. "Services/User"
+	 * @param    event         event, e.g. "afterUpdate"
+	 * @param    array         array of event specific parameters
+	 */
+	public function handleEvent($a_component, $a_event, $a_parameter) {
+	    if($a_component == 'Services/Authentication' && $a_event == 'afterLogin')
+	    {
+	    	global $DIC;
+	    	$rbac = $DIC->rbac();
+	    	$usr_id = $DIC->user()->getId();
+	    	$roles = $rbac->review()->assignedRoles($usr_id);
+
+	    	if (!in_array('2', $roles)) {
+	    		do {
+		    		$ref_id = $rbac->review()->getFoldersAssignedToRole(array_pop($roles), true);
+	    			$node = $DIC->repositoryTree()->getNodeData($ref_id[0]);
+	    			file_put_contents('test.txt', serialize($roles), FILE_APPEND);
+	    		} while ($node['type'] != 'crs' && isset($roles[0]));
+	    	
+	    		if ($node['type'] == 'crs') {
+		    		$DIC->ctrl()->redirectToURL(ilLink::_getLink($node['ref_id']));
+	    		}
+	    	}
+	    }
+	}
+
+	
+	/**
+	 * @return string
+	 */
+	public function getPluginName() {
+		return self::PLUGIN_NAME;
+	}
+
+}
+
+?>
---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+$id = 'xscr';
+$version = '1.0.0';
+$ilias_min_version = '5.3';
+$ilias_max_version = '5.3.999';
+$responsible = 'Stephan Winiker';
+$responsible_mail = 'webmaster@subclauses.net';
+?>
